df19fd54cddfc5510ba381d39924713f388011b8
MOHANAPRASATH S <mohanaprasath.s2023eee@sece.ac.in>
2025-08-04 11:36:00 +0530

    hi

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@next/next/recommended',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 13,
+    sourceType: 'module',
+  },
+  plugins: ['react'],
+  rules: {
+    // Customize your rules here
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+};
